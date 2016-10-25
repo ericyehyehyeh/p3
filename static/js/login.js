693730012694349aad12e21802318f602f2b97d4
jackfmoore <jackfm@umich.edu>
2016-10-25 04:41:02 -0400

    Add files via upload

--- static/js/login.js
+++ static/js/login.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-    //window.alert("function worked");
     $("#loginForm").submit(function(event) {
-    //window.alert("2");
     var formData = $("#loginForm").serialize();
     var url = "http://localhost:3000/gu4wdnfe/p3/api/v1/login";
     $.ajax({
@@ -11,16 +9,28 @@ $(document).ready(function() {
         success : function(data) {
             var obj = JSON.parse(data);
             var username = obj.username;
+            var error = obj.error;
             console.log("username " + username + " has been logged in");
                 if (username) {
                     location.assign("http://localhost:3000/gu4wdnfe/p3/");
                 }
-
-
+                else if (error) {
+                    alert(error)
+                }
         },
-            error : function() {
-                window.alert("error with login");
-            }
+        error : function(response, status, error) {
+            //SOME WORK TO DO WITH ERROR CHECKING
+            //ALSO HAVE TO WORK ON REPRESENTING ERROR IN LOGIN.HTML, SHOULD ONLY HAVE TO DO AN APPEND FUNCTION TO A <P> TAG
+
+            /*var responseArray = JSON.parse(response.responseText);
+            var firstMessageJSON = reponseArray[0];
+            alert(firstMessageJSON);
+            var responseMessage = JSON.parse(reponseArray.errors[0]);
+            //var obj = responseMessage.message;
+            //alert(reponse);
+            //alert(response);
+            alert(response.responseText);*/
+        }
         });
 
         event.preventDefault();
