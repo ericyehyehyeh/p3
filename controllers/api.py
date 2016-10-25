675ed93ff1987f19067993dd16a71b572b9175bd
jackfmoore <jackfm@umich.edu>
2016-10-25 03:17:33 -0400

    Add files via upload

--- controllers/api.py
+++ controllers/api.py
@@ -5,10 +5,22 @@ import os
 
 api = Blueprint('api', __name__, template_folder='templates', url_prefix='/gu4wdnfe/p3')
 
-@api.route('/testroute/<testvariable>')
-def test_route(testvariable):
-	return testvariable
 
-@api.route('/api/v1/login')
+@api.route('/api/v1/login', methods=['POST'])
 def login_route():
-	
+	print "login_route achieved"
+	#dealWithPostData(request.get_json());
+	username = request.form['username'];
+	#username = "test"
+	print "username inputted: " + username;
+	password = request.form['password'];
+	print "password inputted: " + password;
+
+
+
+	#check if valid function
+	#errorType = "invalid username";
+	if True:
+		return jsonify(username=username,password=password)
+	else:
+		return jsonify({'error': errorType})
