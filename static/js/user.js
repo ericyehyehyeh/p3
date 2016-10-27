ee599901e85663060f158eac7edfa801c3193093
swirski <swirski@umich.edu>
2016-10-27 14:38:36 -0400

    Update user.js

--- static/js/user.js
+++ static/js/user.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     //EDIT ROUTE
     $("#update_user").submit(function(event) {
         event.preventDefault();
-        var url = "http://localhost:3000/gu4wdnfe/p3/api/v1/user";
+        var url = "http://class3.eecs.umich.edu:4550/gu4wdnfe/p3/api/v1/user";
 
         $.ajax({
             type: "GET",
@@ -81,7 +81,7 @@ $(document).ready(function() {
                         console.log("it worked! put");
                         console.log(data);
                         console.log("Successfully updated user information!")
-                        window.location.replace("http://localhost:3000/gu4wdnfe/p3/login");
+                        window.location.replace("http://class3.eecs.umich.edu:4550/gu4wdnfe/p3/login");
                     },
 
                     error : function(response) {
@@ -153,7 +153,7 @@ $(document).ready(function() {
     //NEW USER ROUTE
     $("#new_user").submit(function(event) {
         event.preventDefault();
-        var url = "http://localhost:3000/gu4wdnfe/p3/api/v1/user";
+        var url = "http://class3.eecs.umich.edu:4550/gu4wdnfe/p3/api/v1/user";
 
         var username = document.getElementById("new_username_input").value;
         var firstname = document.getElementById("new_firstname_input").value;
@@ -238,7 +238,7 @@ $(document).ready(function() {
             success : function(data) {
                 console.log("it worked! post");
                 console.log(data);
-                window.location.replace("http://localhost:3000/gu4wdnfe/p3/api/v1")
+                window.location.replace("http://class3.eecs.umich.edu:4550/gu4wdnfe/p3/api/v1")
                 },
             error : function(response) {
                 var response2 = JSON.parse(response.responseText);
