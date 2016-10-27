25d975cae714e7161663c9042226b6fbc5608d8c
akao14 <anikakao@umich.edu>
2016-10-26 22:10:21 -0400

    Add files via upload

--- static/js/single_page.js
+++ static/js/single_page.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
         method = 'pic';
     }
 
+    var id = window.location.href.split('=')[1];
+
     $.ajax({
         type: "GET",
         contentType: "application/json; charset=UTF-8",
@@ -166,6 +168,190 @@ $(document).ready(function() {
                             });
                         }
 
+                        page.appendChild(image_element);
+                        page.appendChild(caption_element);
+                        page.appendChild(prev_element);
+                        page.appendChild(next_element);
+                        page.appendChild(input_header);
+                        page.appendChild(caption_input);
+
+
+                    }
+
+
+
+                    },
+                error : function(response) {
+                    var code = response.status;
+                    var response = JSON.parse(response);
+                    var my_errors = response['errors'];
+                    var error_message = "";
+                    var page = document.getElementById("content");
+
+                    if (code == 403){
+                        error_message = my_errors['message'];
+                        var error_para = document.createElement("p");
+                        error_para.setAttribute("class", "error");
+                        var error = document.createTextNode(error_message);
+                        error_para.appendChild(error);
+                        page.appendChild(error_para);
+                    }        
+                }
+            });
+
+
+
+
+
+        }, 
+        errror: function(response){
+            var code = response.status;
+            var response = JSON.parse(response);
+            var my_errors = response['errors'];
+            var error_message = "";
+            var page = document.getElementById("content");
+
+            if (code == 401){
+                $.ajax({
+                    type: "GET", 
+                    contentType: "application/json; charset=UTF-8",
+                    data: JSON.stringify(),
+                    url: "http://localhost:3000/gu4wdnfe/p3/api/v1/" + method,
+                    success : function(data) {
+                        if (method == 'album'){
+
+                        var access = data['access']; 
+                        var albumid = data['albumid'];
+                        var created = data['created'];
+                        var lastupdated = data['lastupdated'];
+                        var photos = data['pics'];
+                        var title = data['title'];
+                        var username = data['username'];
+
+
+                        var page_title = document.createElement("h3");
+                        var album_title = document.createTextNode(title + "-" + username);
+                        page_title.appendChild(album_title);
+
+
+                        var album_edit = document.createElement("LINK");
+                        var edit_link = document.createTextNode("/gu4wdnfe/p3/album/edit?albumid="+albumid);
+                        album_edit.appendChild(edit_link);
+
+
+                        var page = document.getElementById("content");
+                        page.appendChild(page_title);
+                        page.appendChild(album_edit);
+
+                        for (i = 0; i < photos.length; i++){
+
+                            var caption = photo['caption']; 
+                            var sequencenum = photo['sequencenum'];
+                            var picid = photo['picid'];
+                            var format = photo['format'];
+                            var date = photo['date'];
+                            var albumid = photo['albumid'];
+
+                            var image_element = document.createElement("IMG");
+                            image_element.src = "/static/images/" + picid + "." + format;
+                            image_element.width = "200";
+                            image_element.height = "200";
+                            image_element.onClick = window.open("/gu4wdnfe/p3/pic?picid=" + picid);
+
+                            var caption_element = document.createElement("p");
+                            var pic_caption = document.createTextNode(caption);
+                            caption_element.appendChild(pic_caption);
+
+                            var date_element = document.createElement("h6");
+                            var pic_date = document.createTextNode("Date:" + date);
+                            date_element.appendChild(date);
+
+                            page.appendChild(image_element);
+                            page.appendChild(caption_element);
+                            page.appendChild(date_element);
+                            }
+                        }
+                    
+
+                    else if (method == 'pic'){
+                        var albumid = data['albumid'];
+                        var caption = data['caption'];
+                        var format = data['format'];
+                        var next = data['next'];
+                        var prev = data['prev'];
+                        var picid = data['picid'];
+
+
+
+
+                        var image_element = document.createElement("IMG");
+                        image_element.src = "/static/images/" + picid + "." + format;
+
+                        var caption_element = document.createElement("p");
+                        var pic_caption = document.createTextNode(caption);
+                        caption_element.appendChild(pic_caption);
+
+                        var prev_element = document.createElement("h6");
+                        var prev_pic = document.createTextNode("Previous");
+                        prev_pic.onClick = window.open("/gu4wdnfe/p3/pic?picid=" + prev);
+                        prev_element.appendChild(prev_pic);
+
+                        var next_element = document.createElement("h6");
+                        var next_pic = document.createTextNode("Next");
+                        next_pic.onClick = window.open("/gu4wdnfe/p3/pic?picid=" + next);
+                        next_element.appendChild(next_pic);
+
+
+                        if (next == 1){
+                            next_pic.onClick = window.open("/gu4wdnfe/p3/pic?picid=" + picid);
+                        }
+                        if (prev == -1){
+                            prev_pic.onClick = window.open("/gu4wdnfe/p3/pic?picid=" + picid);
+                        }
+
+
+                        var input_header = document.createElement("h6");
+                        var header = document.createTextNode("Edit Caption:");
+                        input_header.appendChild(header);
+
+                        var caption_input = document.createElement("INPUT");
+                        caption_input.setAttribute("type", "text");
+                        caption_input.setAttribute("id", "pic_caption_input")
+
+
+                        document.getElementById("pic_caption_input").onkeypressed = function() {
+                            $.ajax({
+                                type: "PUT", 
+                                contentType: "application/json; charset=UTF-8",
+                                data: JSON.stringify({                            
+                                    "caption": caption, 
+                                    "picid": picid, 
+                                    "format": format, 
+                                    "albumid": albumid, 
+                                    "next": next, 
+                                    "prev": prev
+                                }), 
+                                url: "http://localhost:3000/gu4wdnfe/p3/api/v1/pic/"+picid,
+                                success : function(data) {
+                                    },
+                                error : function(response) {
+                                    var response = JSON.parse(response);
+                                    var my_errors = response['errors'];
+                                    var error_message = "";
+                                    var page = document.getElementById("content");
+
+                                    for (i = 0; i < my_errors.length; i++){
+                                        error_message = my_errors[i]['message'];
+
+                                        var error_para = document.createElement("p");
+                                        error_para.setAttribute("class", "error");
+                                        var error = document.createTextNode(error_message);
+                                        error_para.appendChild(error);
+                                        page.appendChild(error_para);
+                                    }
+                            });
+                        }
+
 
 
 
@@ -182,44 +368,26 @@ $(document).ready(function() {
 
 
                     },
-                error : function(response) {
-                    var response = JSON.parse(response);
-                    var my_errors = response['errors'];
-                    var error_message = "";
-                    var page = document.getElementById("content");
+                    error : function(response) {
+                        var response = JSON.parse(response);
+                        var my_errors = response['errors'];
+                        var error_message = "";
+                        var page = document.getElementById("content");
 
-                    for (i = 0; i < my_errors.length; i++){
-                        error_message = my_errors[i]['message'];
+                        for (i = 0; i < my_errors.length; i++){
+                            error_message = my_errors[i]['message'];
 
-                        var error_para = document.createElement("p");
-                        error_para.setAttribute("class", "error");
-                        var error = document.createTextNode(error_message);
-                        error_para.appendChild(error);
-                        page.appendChild(error_para);
-                    }         
-                }
-            });
-
-
-
+                            var error_para = document.createElement("p");
+                            error_para.setAttribute("class", "error");
+                            var error = document.createTextNode(error_message);
+                            error_para.appendChild(error);
+                            page.appendChild(error_para);
+                        }         
+                    }
+                    });
+            }            
 
 
-        }, 
-        errror: function(response){
-            var response = JSON.parse(response);
-            var my_errors = response['errors'];
-            var error_message = "";
-            var page = document.getElementById("content");
-
-            for (i = 0; i < my_errors.length; i++){
-                error_message = my_errors[i]['message'];
-
-                var error_para = document.createElement("p");
-                error_para.setAttribute("class", "error");
-                var error = document.createTextNode(error_message);
-                error_para.appendChild(error);
-                page.appendChild(error_para);
-                }
             }
     });
 
--- static/js/user.js
+++ static/js/user.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
     //EDIT ROUTE
     $("#edit_submit").submit(function(event) {
-        var url = "http://localhost:3000/gu4wdnfe/p3/api/v1/user/edit";
+        var url = "http://localhost:3000/gu4wdnfe/p3/api/v1/user";
 
         $.ajax({
             type: "GET",
@@ -12,20 +12,68 @@ $(document).ready(function() {
             url: url,
 
             success : function(data) {
-                console.log("it worked! get");
-                console.log(data);
-                username = data['username'];
+                var firstname = document.getElementById("update_firstname_input");
+                var lastname = document.getElementById("update_lastname_input");
+                var email = document.getElementById("update_email_input");
+                var password1 = document.getElementById("update_password1_input");
+                var password2 = document.getElementById("update_password2_input");
+
+
+                var page = document.getElementById("content");
+                var error_para = document.createElement("p");
+                error_para.setAttribute("class", "error");
+                var error = "";
+
+
+                var re_underscore = /^[a-zA-Z0-9_]+$/;
+                if (password1.length < 8){
+                    error = document.createTextNode("Passwords must be at least 8 characters long");
+                    error_para.appendChild(error);
+                }
+                var re_letternum = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
+                if (!re_letternum.text(password1)){
+                    error = document.createTextNode("Passwords must contain at least one letter and one number");
+                    error_para.appendChild(error);
+                }
+                if (!re_underscore.test(password1)){
+                    error = document.createTextNode("Passwords may only contain letters, digits, and underscores");
+                    error_para.appendChild(error);
+                }
+                if (password1 != password2){
+                    error = document.createTextNode("Passwords do not match");
+                    error_para.appendChild(error);
+                }
+                var re_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if (!re_email.text(email)){
+                    error = document.createTextNode("Email address must be valid");
+                    error_para.appendChild(error);
+                }
+                if (firstname.length > 20){
+                    error = document.createTextNode("Firstname must be no longer than 20 characters");
+                    error_para.appendChild(error);
+                }
+                if (lastname.length > 20){
+                    error = document.createTextNode("Lastname must be no longer than 20 characters");
+                    error_para.appendChild(error);
+                }
+                if (email.length > 40){
+                    error = document.createTextNode("Email must be no longer than 40 characters");
+                    error_para.appendChild(error);
+                }
+
+
+                page.appendChild(error_para);
 
                 $.ajax({
                     type: "PUT", 
                     contentType: "application/json; charset=UTF-8",
                     data: JSON.stringify({
-                        "username" : document.getElementById("new_username_input"),
-                        "firstname" : document.getElementById("new_firstname_input"),
-                        "lastname" :  document.getElementById("new_lastname_input"),
-                        "email" : document.getElementById("new_email_input"),
-                        "password1" : document.getElementById("new_password1_input"),
-                        "password2" : document.getElementById("new_password2_input")
+                        "username" : document.getElementById("update_username_input"),
+                        "firstname" : document.getElementById("update_firstname_input"),
+                        "lastname" :  document.getElementById("update_lastname_input"),
+                        "email" : document.getElementById("update_email_input"),
+                        "password1" : document.getElementById("update_password1_input"),
+                        "password2" : document.getElementById("update_password2_input")
                     }), 
                     url: url,
 
@@ -52,12 +100,6 @@ $(document).ready(function() {
                             }         
                         }
                 });
-
-                    //var page_title = document.createElement("h3");
-                    //var album_title = document.createTextNode(title);
-                    //page_title.appendChild(album_title);
-                    //var element = document.getElementById("content");
-                    //element.appendChild(page_title);
                 },
             error : function(response) {
                 var response = JSON.parse(response);
@@ -82,7 +124,74 @@ $(document).ready(function() {
 
     //NEW USER ROUTE
     $("#new_submit").submit(function(event) {
+        event.preventDefault();
         var url = "http://localhost:3000/gu4wdnfe/p3/api/v1/user";
+
+        var username = document.getElementById("new_username_input");
+        var firstname = document.getElementById("new_firstname_input");
+        var lastname = document.getElementById("new_lastname_input");
+        var email = document.getElementById("new_email_input");
+        var password1 = document.getElementById("new_password1_input");
+        var password2 = document.getElementById("new_password2_input");
+
+
+        var page = document.getElementById("content");
+        var error_para = document.createElement("p");
+        error_para.setAttribute("class", "error");
+        var error = "";
+
+
+        if (username.length < 3) {
+            error = document.createTextNode("Usernames must be at least 3 characters long");
+            error_para.appendChild(error);
+        }
+        var re_underscore = /^[a-zA-Z0-9_]+$/;
+        if (!re_underscore.test(username)){
+            error = document.createTextNode("Usernames may only contain letters, digits, and underscores");
+            error_para.appendChild(error);
+        }
+        if (password1.length < 8){
+            error = document.createTextNode("Passwords must be at least 8 characters long");
+            error_para.appendChild(error);
+        }
+        var re_letternum = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
+        if (!re_letternum.text(password1)){
+            error = document.createTextNode("Passwords must contain at least one letter and one number");
+            error_para.appendChild(error);
+        }
+        if (!re_underscore.test(password1)){
+            error = document.createTextNode("Passwords may only contain letters, digits, and underscores");
+            error_para.appendChild(error);
+        }
+        if (password1 != password2){
+            error = document.createTextNode("Passwords do not match");
+            error_para.appendChild(error);
+        }
+        var re_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re_email.text(email)){
+            error = document.createTextNode("Email address must be valid");
+            error_para.appendChild(error);
+        }
+        if (username.length > 20){
+            error = document.createTextNode("Username must be no longer than 20 characters");
+            error_para.appendChild(error);
+        }
+        if (firstname.length > 20){
+            error = document.createTextNode("Firstname must be no longer than 20 characters");
+            error_para.appendChild(error);
+        }
+        if (lastname.length > 20){
+            error = document.createTextNode("Lastname must be no longer than 20 characters");
+            error_para.appendChild(error);
+        }
+        if (email.length > 40){
+            error = document.createTextNode("Email must be no longer than 40 characters");
+            error_para.appendChild(error);
+        }
+        
+
+        page.appendChild(error_para);
+
 
         $.ajax({
             type: "POST", 
